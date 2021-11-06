a961f9580b2aa571621e67a56037afead82cf29f
Stefan Cirstea <stefan.sc.sc@gmail.com>
2021-11-06 11:46:07 +0200

    fix loading

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
 import HomeScreen from "./src/screens/HomeScreen";
 import SplashScreen from './src/screens/SplashScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import LoadingScreen from './src/screens/LoadingScreen';
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 
 
@@ -42,6 +44,17 @@ const App = () => {
           component={HomeScreen}
         >
         </Stack.Screen>
+        <Stack.Screen
+          name="LoadingScreen"
+          component={LoadingScreen}
+          options={{ headerShown: false }} 
+        >
+        </Stack.Screen>
+        {/* <Stack.Screen
+          name="ProfileScreen"
+          component={ProfileScreen}
+        >
+        </Stack.Screen> */}
         <Stack.Screen
           name="Login"
           component={LoginScreen}
